436738e46ac9f18f4d9d693fabaed3f3c144683a
taesun_lee <taesun_lee@tmax.co.kr>
2021-01-28 14:04:14 +0900

    temp

--- webhook/validation.go
+++ webhook/validation.go
@@ -1,7 +1,34 @@
 package webhook
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"k8s.io/apiserver/pkg/admission"
+)
 
 func HandleValidate(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func validate(ctx context.Context, a admission.Attributes, o admission.ObjectInterfaces) error {
+
+	pod := a.GetObject().(*api.Pod)
+
+	if pod.Spec.Tolerations != nil {
+		targetResourcesSet := GetTargetResourcesSet()
+
+		for _, toleration := range pod.Spec.Tolerations {
+			if targetResourcesSet.Contains(toleration.key) {
+				return fmt.Errorf("System Toleration Key:", whitelistScope)
+			}
+		}
+	}
+
+	return nil
+}
+
+func getExtendResourceSetUsedByPod(pod *api.Pod) {
 
 }
