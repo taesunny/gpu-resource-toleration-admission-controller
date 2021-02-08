12f01026b5b83ae275430fddd6c36f4cb1fef69d
Kwang Hun <mm9139@naver.com>
2021-02-08 15:39:38 +0900

    test cases structure로 변경

--- webhook/validation_test.go
+++ webhook/validation_test.go
@@ -14,42 +14,50 @@ import (
 
 func TestValidate(t *testing.T) {
 	uid := types.UID("12D3FG")
-
-	request := admissionv1.AdmissionReview{
-		TypeMeta: metav1.TypeMeta{Kind: "pods", APIVersion: "v1"},
-		Request:  &admissionv1.AdmissionRequest{UID: uid, Resource: podResource},
+	cases := []struct {
+		in   admissionv1.AdmissionReview
+		want string
+	}{
+		{
+			in: admissionv1.AdmissionReview{
+				TypeMeta: metav1.TypeMeta{Kind: "pods", APIVersion: "v1"},
+				Request:  &admissionv1.AdmissionRequest{UID: uid, Resource: podResource},
+			},
+			want: `{"response":{"uid":"` + string(uid) + `","allowed":true}}`,
+		},
 	}
 
-	pbytes, err := json.Marshal(request)
-	if err != nil {
-		t.Errorf("marshaling response: %v", err)
-	}
-	buff := bytes.NewBuffer(pbytes)
+	for _, c := range cases {
+		pbytes, err := json.Marshal(c.in)
+		if err != nil {
+			t.Errorf("marshaling response: %v", err)
+		}
+		buff := bytes.NewBuffer(pbytes)
 
-	req, err := http.NewRequest("POST", "/validate", buff)
-	if err != nil {
-		t.Error()
-	}
-	req.Header.Set("Content-Type", "application/json")
+		req, err := http.NewRequest("POST", "/validate", buff)
+		if err != nil {
+			t.Error()
+		}
+		req.Header.Set("Content-Type", "application/json")
 
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(HandleValidate)
+		// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(HandleValidate)
 
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
-	handler.ServeHTTP(rr, req)
+		// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+		// directly and pass in our Request and ResponseRecorder.
+		handler.ServeHTTP(rr, req)
 
-	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
+		// Check the status code is what we expect.
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
 
-	// Check the response body is what we expect.
-	expected := `{"response":{"uid":"` + string(uid) + `","allowed":true}}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+		// Check the response body is what we expect.
+		if rr.Body.String() != c.want {
+			t.Errorf("handler returned unexpected body: got %v want %v",
+				rr.Body.String(), c.want)
+		}
 	}
 }
